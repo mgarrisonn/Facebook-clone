f92cd467e27a2d43de5e5fbba6240168ccb6e4db
michael garrison <mgarrison.sa@gmail.com>
2020-12-22 18:59:38 -0600

    set up gitignore

---
+++ src/main/java/com/facebookclone/facbookclone/FacbookcloneApplication.java
@@ -0,0 +1,13 @@
+package com.facebookclone.facbookclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacbookcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacbookcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/facebookclone/facbookclone/FacbookcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facebookclone.facbookclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacbookcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
