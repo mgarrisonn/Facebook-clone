e99b0cf7a176e9a12e75633eb387605bbc06a1c5
michael garrison <mgarrison.sa@gmail.com>
2020-12-29 23:51:25 -0600

    comment model

---
+++ src/main/java/com/facebookclone/facbookclone/models/Comment.java
@@ -0,0 +1,46 @@
+package com.facebookclone.facbookclone.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    @JsonBackReference
+    private Post parentPost;
+
+    public Comment(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getParentPost() {
+        return parentPost;
+    }
+
+    public void setParentPost(Post parentPost) {
+        this.parentPost = parentPost;
+    }
+}
